d047fc082e2a78a49b52c03e2ecdff1102f96f7f
MyseriousKnight <63143500+AeneonLXC@users.noreply.github.com>
2022-08-12 23:32:12 +0800

    更改了主函数
    将主函数的示例进行注释

--- baidu_api.py
+++ baidu_api.py
@@ -107,19 +107,19 @@ class BaiduApi:
         print("结果:{}".format(response_str))
         return response_json
         
-if __name__ == "__main__":
-    PARAMS = {"top_num": 3}
-    key = pd.read_excel(r"E:\MysteriousKnight\github_repository\VOC2007\apiKey.xls")
-    key = np.array(key)
-    MODEL_API_URL = key[0,1]
-    ACCESS_TOKEN = key[1,1]
-    API_KEY = key[2,1]
-    SECRET_KEY = key[3,1]
-    app = BaiduApi(PARAMS, MODEL_API_URL,
-                   ACCESS_TOKEN,
-                   API_KEY,SECRET_KEY)
+# if __name__ == "__main__":
+#     PARAMS = {"top_num": 3}
+#     key = pd.read_excel(r"E:\MysteriousKnight\github_repository\VOC2007\apiKey.xls")
+#     key = np.array(key)
+#     MODEL_API_URL = key[0,1]
+#     ACCESS_TOKEN = key[1,1]
+#     API_KEY = key[2,1]
+#     SECRET_KEY = key[3,1]
+#     app = BaiduApi(PARAMS, MODEL_API_URL,
+#                    ACCESS_TOKEN,
+#                    API_KEY,SECRET_KEY)
 
-    img_path = "E:/MysteriousKnight/github_repository/VOC2007/Image/mvi_4531_000008.jpg"
-    app.img_deticion(img_path)
+#     img_path = "E:/MysteriousKnight/github_repository/VOC2007/Image/mvi_4531_000008.jpg"
+#     app.img_deticion(img_path)
     
 
